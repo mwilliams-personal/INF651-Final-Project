fd140a76d2b5dc9a03a1fe80571e72fa88ddbec6
Matt Williams <lpugfan2004@gmail.com>
2020-11-21 19:33:05 -0500

    added extended forecast and working images for import

--- importOW.js
+++ importOW.js
@@ -6,36 +6,47 @@ document.getElementById("ImportWeather").onclick = function()
     const state = document.getElementById("stateCode").value;
     const zip = document.getElementById("zipCode").value;
     
-    const fetUrl = formatAPIURL(city, state, zip);
+    const fetCurrUrl = formatCurrAPIURL(city, state, zip);
         
-    if(fetUrl != "false")
+    if(fetCurrUrl != "false")
     {
-        console.log(fetUrl);
-        ImportTest(fetUrl);
+        console.log(fetCurrUrl);
+        ImportCurrent(fetCurrUrl);
     }
 }
 
-async function ImportTest(URL)
+async function ImportCurrent(URL)
 {
     const response = await fetch(URL);
     const theData = await response.json();
 
     drawCurrentWeather(theData);
+    ImportExtended(theData.coord.lat,theData.coord.lon);
 }
 
-function formatAPIURL(city, state, zip)
+async function ImportExtended(lat, lon)
+{
+    const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat +'&lon='+lon+'&exclude=minutely,hourly,alerts&units=imperial&appid=' + WEATHER_API_KEY;
+    console.log(URL);
+    const response = await fetch(URL);
+    const theData = await response.json();
+
+    drawFiveDayWeather(theData);
+}
+
+function formatCurrAPIURL(city, state, zip)
 {
     if(city != "" && state != "")
     {
-        return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + ",us&appid=" + WEATHER_API_KEY;
+        return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + ",us&units=imperial&appid=" + WEATHER_API_KEY;
     }
     else if(city != "")
     {
-        return "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=" + WEATHER_API_KEY;
+        return "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial&appid=" + WEATHER_API_KEY;
     }
     else if(zip != "")
     {
-        return "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&appid=" + WEATHER_API_KEY;
+        return "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&units=imperial&appid=" + WEATHER_API_KEY;
     }
     else 
     {
@@ -47,28 +58,68 @@ function drawCurrentWeather(weatherData)
 {
     document.getElementById('current-image').innerHTML = "";
     const newImage = document.createElement('img');
-    //create img src url
     const iconSource = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
-    //set img attributes
     newImage.setAttribute('alt', weatherData.weather.description); 
     newImage.setAttribute('src', iconSource);
     newImage.setAttribute('style', "width: 10vh;");
-    //append image to the parent div
     document.getElementById('current-image').appendChild(newImage);
 
-    var fahrenheit = Math.round(((parseFloat(weatherData.main.temp)-273.15)*1.8)+32); 
-    var lowfar = Math.round(((parseFloat(weatherData.main.temp_min)-273.15)*1.8)+32); 
-    var maxfar = Math.round(((parseFloat(weatherData.main.temp_max)-273.15)*1.8)+32); 
-
-    document.getElementById('temp').innerHTML = "Current: " + fahrenheit + '&deg;';
+    document.getElementById('temp').innerHTML = "Current: " + parseInt(weatherData.main.temp) + '&deg;';
     document.getElementById('humidity').innerHTML = "Humidity: " + weatherData.main.humidity + "%";
-    document.getElementById('mintemp').innerHTML = "Low: " + lowfar + '&deg;';
-    document.getElementById('maxtemp').innerHTML = "High: " + maxfar + '&deg;';
+    document.getElementById('mintemp').innerHTML = "Low: " + parseInt(weatherData.main.temp_min) + '&deg;';
+    document.getElementById('maxtemp').innerHTML = "High: " + parseInt(weatherData.main.temp_max) + '&deg;';
     document.getElementById('windspeed').innerHTML = "Wind speed: " + weatherData.wind.speed + " mph";
     document.getElementById('location').innerHTML = "Location: " + weatherData.name;
 }
 
-drawFiveDayWeather(weatherData)
+function drawFiveDayWeather(weatherData)
 {
+    /*<div id="div-ext-1" class="five-day-iconAndText">
+                    <div id="ext-1-img"></div>
+                    <h3 id="ext-1-low"></h3>
+                    <h3 id="ext-1-high"></h3>
+                    <h3 id="ext-1-day"></h3>
+                </div>*/
+
+    //
+
+    //Day 1
+    document.getElementById('ext-1-low').innerHTML = "Low: " + parseInt(weatherData.daily[0].temp.min) + '&deg;';
+    document.getElementById('ext-1-high').innerHTML = "High: " + parseInt(weatherData.daily[0].temp.max) + '&deg;';
+    document.getElementById('ext-1-day').innerHTML = DayOfTheWeek(1);
     
+    //Day 2
+    document.getElementById('ext-2-low').innerHTML = "Low: " + parseInt(weatherData.daily[1].temp.min) + '&deg;';
+    document.getElementById('ext-2-high').innerHTML = "High: " + parseInt(weatherData.daily[1].temp.max) + '&deg;';
+    document.getElementById('ext-2-day').innerHTML = DayOfTheWeek(2);
+    
+    //Day 3
+    document.getElementById('ext-3-low').innerHTML = "Low: " + parseInt(weatherData.daily[2].temp.min) + '&deg;';
+    document.getElementById('ext-3-high').innerHTML = "High: " + parseInt(weatherData.daily[2].temp.max) + '&deg;';
+    document.getElementById('ext-3-day').innerHTML = DayOfTheWeek(3);
+    
+    //Day 4
+    document.getElementById('ext-4-low').innerHTML = "Low: " + parseInt(weatherData.daily[3].temp.min) + '&deg;';
+    document.getElementById('ext-4-high').innerHTML = "High: " + parseInt(weatherData.daily[3].temp.max) + '&deg;';
+    document.getElementById('ext-4-day').innerHTML = DayOfTheWeek(4);
+    
+    //Day 5
+    document.getElementById('ext-5-low').innerHTML = "Low: " + parseInt(weatherData.daily[4].temp.min) + '&deg;';
+    document.getElementById('ext-5-high').innerHTML = "High: " + parseInt(weatherData.daily[4].temp.max) + '&deg;';
+    document.getElementById('ext-5-day').innerHTML = DayOfTheWeek(5);
+}
+
+function DayOfTheWeek(day)
+{
+    var d = new Date();
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    if(d.getDay() + day > 6)
+    {
+        return weekday[(d.getDay() + day) - 6];
+    }
+    else
+    {
+        return weekday[d.getDay() + day];
+    }
 }
