55f6a295907e0257b721c997f71a78ee97f44ddc
Matt Williams <lpugfan2004@gmail.com>
2020-11-22 23:50:34 -0500

    aria support and fully functioning in chromvox, rebuilt base HTML for support proper accesbility, tab ordering added to elements

--- importOW.js
+++ importOW.js
@@ -4,7 +4,35 @@ defaultLoad();
 
 document.getElementById("ImportWeather").onclick = function()
 {
-    
+    LoadBasedOnEntry();
+}
+
+document.getElementById("cityName").addEventListener('keypress', function (e) 
+{
+    if (e.key === 'Enter') 
+    {
+        LoadBasedOnEntry();
+    }
+});
+
+document.getElementById("stateCode").addEventListener('keypress', function (e) 
+{
+    if (e.key === 'Enter') 
+    {
+        LoadBasedOnEntry();
+    }
+});
+
+document.getElementById("zipCode").addEventListener('keypress', function (e) 
+{
+    if (e.key === 'Enter') 
+    {
+        LoadBasedOnEntry();
+    }
+});
+
+function LoadBasedOnEntry()
+{
     const city = document.getElementById("cityName").value;
     const state = document.getElementById("stateCode").value;
     const zip = document.getElementById("zipCode").value;
@@ -13,16 +41,17 @@ document.getElementById("ImportWeather").onclick = function()
         
     if(fetCurrUrl != "false")
     {
-        console.log(fetCurrUrl);
         ImportCurrent(fetCurrUrl);
+        if(localStorage.getItem('LOCATIONSAVED') == null)
+        {
+            document.getElementById('DefaultSave').setAttribute('aria-label', "Save " + myStorage.getItem('LOCATIONRECENT') + " as default location");
+        }
     }
-    
 }
 
 document.getElementById("DefaultSave").onclick = function()
 {
     myStorage = window.localStorage;
-    console.log(localStorage.getItem(URL));
 
     if(document.getElementById('DefaultSave').innerHTML === "Save as default location")
     {
@@ -32,16 +61,20 @@ document.getElementById("DefaultSave").onclick = function()
         const zip = document.getElementById("zipCode").value;
         
         const fetCurrUrl = formatCurrAPIURL(city, state, zip);
+        
+        console.log("test");
 
         //Now save the fetCurrUrl as local data
         localStorage.setItem(URL, fetCurrUrl);
-        console.log(localStorage.getItem(URL));
+        localStorage.setItem('LOCATIONSAVED', city+state+zip);
         document.getElementById('DefaultSave').innerHTML = "Clear default location";
+        document.getElementById('DefaultSave').setAttribute('aria-label', "Clear default location, currently set to" + localStorage.getItem('LOCATIONSAVED'));
     }
     else
     {
-        localStorage.clear();
         document.getElementById('DefaultSave').innerHTML = "Save as default location";
+        document.getElementById('DefaultSave').setAttribute('aria-label', "Save " + myStorage.getItem('LOCATIONRECENT') + " as default location");
+        localStorage.clear();
     }
 }
 
@@ -54,13 +87,14 @@ function defaultLoad()
 
     if(defURL != null)
     {
-        console.log(defURL);
         ImportCurrent(defURL);
         document.getElementById('DefaultSave').innerHTML = "Clear default location";
+        document.getElementById('DefaultSave').setAttribute('aria-label', "Clear default location, currently set to" + localStorage.getItem('LOCATIONSAVED'));
     }
     else
     {
         document.getElementById('DefaultSave').innerHTML = "Save as default location";
+        document.getElementById('DefaultSave').setAttribute('aria-label', "Save " + myStorage.getItem('LOCATIONRECENT') + " as default location");
     }
 }
 
@@ -80,7 +114,6 @@ async function ImportCurrent(URL)
     }
     catch(error)
     {
-        console.log("fetch failed");
         [].forEach.call(document.querySelectorAll('.warningVis'), function (el) {
             el.style.display = 'block';
         });
@@ -90,7 +123,6 @@ async function ImportCurrent(URL)
 async function ImportExtended(lat, lon)
 {
     const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat +'&lon='+lon+'&exclude=minutely,hourly,alerts&units=imperial&appid=' + WEATHER_API_KEY;
-    console.log(URL);
     const response = await fetch(URL);
     const theData = await response.json();
 
@@ -99,6 +131,8 @@ async function ImportExtended(lat, lon)
 
 function formatCurrAPIURL(city, state, zip)
 {
+    localStorage.setItem('LOCATIONRECENT', city+state+zip);
+    
     if(city != "" && state != "")
     {
         return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + ",us&units=imperial&appid=" + WEATHER_API_KEY;
@@ -120,20 +154,26 @@ function formatCurrAPIURL(city, state, zip)
 function drawCurrentWeather(weatherData) 
 {
     const newImage = document.createElement('img');
-    const iconSource = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
+    const iconSource = "https://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
     document.getElementById('current-image').innerHTML = "";
-    newImage.setAttribute('alt', weatherData.weather.description); 
-    newImage.setAttribute('title', weatherData.weather.description); 
+    newImage.setAttribute('alt', weatherData.weather[0].description); 
+    newImage.setAttribute('title', weatherData.weather[0].description); 
     newImage.setAttribute('src', iconSource);
     newImage.setAttribute('style', "width: 10vh;");
     document.getElementById('current-image').appendChild(newImage);
 
-    document.getElementById('temp').innerHTML = "Current: " + parseInt(weatherData.main.temp) + '&deg;';
+    //Current temp
+    document.getElementById('temp').innerHTML = "Current: " + Math.round(weatherData.main.temp) + '&deg;';
+    document.getElementById('temp').setAttribute('aria-label', "Current temperature: " + Math.round(weatherData.main.temp) + ' degrees');
+    //Humidity
     document.getElementById('humidity').innerHTML = "Humidity: " + weatherData.main.humidity + "%";
-    document.getElementById('mintemp').innerHTML = "Low: " + parseInt(weatherData.main.temp_min) + '&deg;';
-    document.getElementById('maxtemp').innerHTML = "High: " + parseInt(weatherData.main.temp_max) + '&deg;';
+    document.getElementById('humidity').setAttribute('aria-label', "Current Humidity: " + weatherData.main.humidity + "  percent");
+    //Windspeed
     document.getElementById('windspeed').innerHTML = "Wind speed: " + weatherData.wind.speed + " mph";
+    document.getElementById('windspeed').setAttribute('aria-label', "Current Wind speed: " + weatherData.wind.speed + " miles per hour");
+    //Location
     document.getElementById('location').innerHTML = "Location: " + weatherData.name;
+    document.getElementById('location').setAttribute('aria-label', "Location: " + weatherData.name);
 
     if(document.getElementById('temp').innerHTML != "")
     {
@@ -145,23 +185,37 @@ function drawCurrentWeather(weatherData)
 
 function drawFiveDayWeather(weatherData)
 {
+    //Current Day Low Temp
+    document.getElementById('mintemp').innerHTML = "Low: " + Math.round(weatherData.daily[0].temp.min) + '&deg;';
+    document.getElementById('mintemp').setAttribute('aria-label', "Today's Low Temperature: " + Math.round(weatherData.daily[0].temp.min) + ' degrees');
+    //Curremt Day High Temp
+    document.getElementById('maxtemp').innerHTML = "High: " + Math.round(weatherData.daily[0].temp.max) + '&deg;';
+    document.getElementById('maxtemp').setAttribute('aria-label', "Today's High Temperature: " + Math.round(weatherData.daily[0].temp.max) + ' degrees');
+
     for(let i = 1; i <= 5; i++)
     {
+        //Image
         document.getElementById("ext-"+i+"-img").innerHTML = "";
-        document.getElementById("ext-"+i+"-img").appendChild(setImageExt(i-1, weatherData));
-        document.getElementById("ext-"+i+"-low").innerHTML = "Low: " + parseInt(weatherData.daily[i-1].temp.min) + '&deg;';
-        document.getElementById("ext-"+i+"-high").innerHTML = "High: " + parseInt(weatherData.daily[i-1].temp.max) + '&deg;';
+        document.getElementById("ext-"+i+"-img").appendChild(setImageExt(i, weatherData));
+        //Low Temp
+        document.getElementById("ext-"+i+"-low").innerHTML = "Low: " + Math.round(weatherData.daily[i].temp.min) + '&deg;';
+        document.getElementById("ext-"+i+"-low").setAttribute('aria-label', "Low: " + Math.round(weatherData.daily[i].temp.min) + ' degrees');
+        //High Temp
+        document.getElementById("ext-"+i+"-high").innerHTML = "High: " + Math.round(weatherData.daily[i].temp.max) + '&deg;';
+        document.getElementById("ext-"+i+"-high").setAttribute('aria-label', "High: " + Math.round(weatherData.daily[i].temp.max) + ' degrees');
+        //Day of the week
         document.getElementById("ext-"+i+"-day").innerHTML = DayOfTheWeek(i);
         document.getElementById("ext-"+i+"-day").setAttribute('alt', DayOfTheWeekFull(i));
+        document.getElementById("ext-"+i+"-day").setAttribute('aria-label', DayOfTheWeekFull(i));
     }
 }
 
 function setImageExt(count, weatherData)
 {
     const newImage = document.createElement('img');
-    const iconSource = "http://openweathermap.org/img/wn/" + weatherData.daily[count].weather[0].icon + "@2x.png";
-    newImage.setAttribute('alt', weatherData.daily[count].weather.description); 
-    newImage.setAttribute('title', weatherData.daily[count].weather.description); 
+    const iconSource = "https://openweathermap.org/img/wn/" + weatherData.daily[count].weather[0].icon + "@2x.png";
+    newImage.setAttribute('alt', weatherData.daily[count].weather[0].description); 
+    newImage.setAttribute('title', weatherData.daily[count].weather[0].description);
     newImage.setAttribute('src', iconSource);
     newImage.setAttribute('style', "width: 10vh;");
     return newImage;
